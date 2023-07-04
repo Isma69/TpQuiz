c0da08e521871081b8a441cb078ed8304eb13776
Isma69 <ismael.bouhassoun@hotmail.fr>
2023-07-04 13:49:48 +0200

    requete sql ajouté

--- htdocs/TpQuiz.sql
+++ htdocs/TpQuiz.sql
@@ -1,26 +1,25 @@
 CREATE TABLE questions (
   id INT PRIMARY KEY,
-  titre VARCHAR(255),
-  contenu TEXT,
-  date_creation DATE,
-  id_utilisateur INT,
-  FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id)
+  title VARCHAR(255),
+  content TEXT,
+  creation_date DATE,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 
-CREATE TABLE utilisateurs (
+CREATE TABLE users (
   id INT PRIMARY KEY,
-  nom VARCHAR(255),
-  email VARCHAR(255)
+  pseudo VARCHAR(255),
 );
 
 
 
 CREATE TABLE score (
   id INT PRIMARY KEY,
-  id_utilisateur INT,
+  user_id INT,
   score INT,
-  FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 
